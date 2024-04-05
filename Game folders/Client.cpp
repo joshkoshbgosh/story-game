c97b5bea63e9e505be65b1f40dd0603424d330b1
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-04-04 23:47:10 -0400

    Update Client.cpp

--- Game folders/Client.cpp
+++ Game folders/Client.cpp
@@ -14,7 +14,6 @@ constexpr int BUFFER_SIZE = 1024;
 std::atomic<bool> has_input(false); // Flag to track if any client has entered input
 std::mutex input_mutex;
 
-
 void clientTask(int client_id) {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
@@ -31,23 +30,21 @@ void clientTask(int client_id) {
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
         std::cerr << "\nInvalid address/ Address not supported for client " << client_id << std::endl;
+        close(sock); // Close the socket before exiting
         return;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         std::cerr << "\nConnection Failed for client " << client_id << std::endl;
+        close(sock); // Close the socket before exiting
         return;
     }
 
     std::string input;
     while (true) {
         std::lock_guard<std::mutex> lock(input_mutex);
-        if (!has_input) {
-            std::cout << "Any client: Enter message (Enter 'QUIT' to leave'): ";
-            has_input=true;
-        } else {
-            std::cout << "Client " << client_id << ": Enter message (Enter 'QUIT' to leave'): ";
-        }
+       
+        std::cout << "Client " << client_id << ": Enter message (Enter 'QUIT' to leave'): ";
 
         std::getline(std::cin, input);
         has_input = true;
@@ -55,11 +52,11 @@ void clientTask(int client_id) {
         // Check if the user wants to quit
         if (input == "QUIT") {
             std::cout <<"Goodbye Player\n";
-            break; // Exit the loop
-            
+            close(sock); // Close the socket before exiting
+            break; // Exit the function
         }
 
-         // Add a space at the end of the input
+        // Add a space at the end of the input
         input += " ";
 
         send(sock, input.c_str(), input.length(), 0);
@@ -75,7 +72,7 @@ int main(int argc, char const *argv[]) {
     std::vector<std::thread> client_threads;
 
     // Number of clients to run concurrently
-    int num_clients = 5;
+    int num_clients = 999;
 
     // Create threads for each client
     for (int i = 0; i < num_clients; ++i) {
